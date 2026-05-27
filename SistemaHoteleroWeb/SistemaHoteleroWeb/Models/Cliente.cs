0f5b9e74ea3404228bb9bc18cf4034f7fe43f666
Marco David <MarcoDavid@hotmail.com>
2026-05-27 02:26:47 -0700

    Crea modelo y dto cliente

---
+++ SistemaHoteleroWeb/SistemaHoteleroWeb/Models/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaHoteleroWeb.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(13, MinimumLength = 12)] // El RFC en México tiene 12 o 13 caracteres
+        public string RFC { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(15)]
+        public string Telefono { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; } = string.Empty;
+    }
+}
