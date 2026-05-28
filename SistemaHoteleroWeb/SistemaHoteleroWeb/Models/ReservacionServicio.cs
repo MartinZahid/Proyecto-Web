9f5ff2441febfe9b1c121f9f5df6669bae8a3d7b
Marco David <MarcoDavid@hotmail.com>
2026-05-28 00:57:50 -0700

    Finaliza Modelos

--- SistemaHoteleroWeb/SistemaHoteleroWeb/Models/ReservacionServicio.cs
+++ SistemaHoteleroWeb/SistemaHoteleroWeb/Models/ReservacionServicio.cs
@@ -1,6 +1,41 @@
-﻿namespace SistemaHoteleroWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SistemaHoteleroWeb.Models
 {
     public class ReservacionServicio
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int ReservacionId { get; set; }
+
+        [ForeignKey("ReservacionId")]
+        public Reservacion? Reservacion { get; set; }
+
+        [Required]
+        public int ClienteId { get; set; }
+
+        [ForeignKey("ClienteId")]
+        public Cliente? Cliente { get; set; }
+
+        [Required]
+        public int ServicioId { get; set; }
+
+        [ForeignKey("ServicioId")]
+        public Servicio? Servicio { get; set; }
+
+        [Required]
+        public DateTime FechaHora { get; set; } = DateTime.Now;
+
+        [Required]
+        public int Cantidad { get; set; } = 1;
+
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Subtotal { get; set; }
+
+        [StringLength(300)]
+        public string? Observaciones { get; set; }
     }
 }
--- SistemaHoteleroWeb/SistemaHoteleroWeb/Models/Servicio.cs
+++ SistemaHoteleroWeb/SistemaHoteleroWeb/Models/Servicio.cs
@@ -1,6 +1,26 @@
-﻿namespace SistemaHoteleroWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SistemaHoteleroWeb.Models
 {
     public class Servicio
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; } = string.Empty;
+
+        [StringLength(300)]
+        public string? Descripcion { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Precio { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Estado { get; set; } = "Activo";
     }
 }
